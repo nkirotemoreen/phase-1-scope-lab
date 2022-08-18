a06de831b6e35dc86e73daa06d55499d88bd1489
moreenisaac <nkirotemoreen70@gmail.com>
2022-08-18 11:28:42 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+//declare variable
+var customerName = "bob";
+const leastFavoriteCustomer = "Billy"
+ function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+ }
+
+ //best custoemr
+ function setBestCustomer(){
+    bestCustomer = 'not bob'
+ }
+ //overwritting globalscope
+ function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+  }
+  function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'bolly'
+  }
